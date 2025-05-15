0face9880034d8f3540601e3da08bba8970ceb60
Samarth-1206 <pawar06samarth14@gmail.com>
2025-05-15 08:34:22 +0530

    Update start.py

--- AaruXMusix/plugins/bot/start.py
+++ AaruXMusix/plugins/bot/start.py
@@ -1,3 +1,42 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from BILI_MUSIC import app
+
+#--------------------------
+
+MUST_JOIN = "GHOULS_SUPPORT"
+#------------------------
+@app.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(app: Client, msg: Message):
+    if not MUST_JOIN:
+        return
+    try:
+        try:
+            await app.get_chat_member(MUST_JOIN, msg.from_user.id)
+        except UserNotParticipant:
+            if MUST_JOIN.isalpha():
+                link = "https://t.me/" + MUST_JOIN
+            else:
+                chat_info = await app.get_chat(MUST_JOIN)
+                link = chat_info.invite_link
+            try:
+                await msg.reply_photo(
+                    photo="https://files.catbox.moe/fu6jk3.jpg", caption=f"๏ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ [๏ sᴜᴘᴘᴏʀᴛ ๏]({link}) ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴄʜᴇᴀᴋ ᴍʏ ғᴇᴀᴛᴜʀᴇs.\n\nᴀғᴛᴇʀ ᴊᴏɪɴ ᴛʜᴇ [๏ ᴄʜᴀɴɴᴇʟ ๏]({link}) ᴄᴏᴍᴇ ʙᴀᴄᴋ ᴛᴏ ᴛʜᴇ ʙᴏᴛ ᴀɴᴅ ᴛʏᴘᴇ /start ᴀɢᴀɪɴ !! ",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("• ᴊᴏɪɴ •", url=link),
+                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/ONE_WAS_SIGMA"),
+                            ]
+                        ]
+                    )
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"๏ ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴀs ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ᴍᴜsᴛ_ᴊᴏɪɴ ᴄʜᴀᴛ ๏: {MUST_JOIN} !")
 import time
 import random
 from pyrogram import filters
@@ -6,10 +45,10 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from AaruXMusix import app
-from AaruXMusix.misc import _boot_
-from AaruXMusix.plugins.sudo.sudoers import sudoers_list
-from AaruXMusix.utils.database import (
+from BILI import app
+from BILI.misc import _boot_
+from BILI.plugins.sudo.sudoers import sudoers_list
+from BILI.utils.database import (
     add_served_chat,
     add_served_user,
     blacklisted_chats,
@@ -17,9 +56,9 @@ from AaruXMusix.utils.database import (
     is_banned_user,
     is_on_off,
 )
-from AaruXMusix.utils.decorators.language import LanguageStart
-from AaruXMusix.utils.formatters import get_readable_time
-from AaruXMusix.utils.inline import help_pannel, private_panel, start_panel
+from BILI.utils.decorators.language import LanguageStart
+from BILI.utils.formatters import get_readable_time
+from BILI.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
@@ -33,7 +72,8 @@ Hina_PICS = [
 "https://telegra.ph/file/ace92d59d19127d2d4e89.jpg",
 "https://telegra.ph/file/a0db46dfacd94e489117b.jpg",
 "https://telegra.ph/file/cd77be2595cdc2fca60a3.jpg",
-"https://telegra.ph/file/632724b3d30c691247c77.jpg",
+"https://telegra.ph/file/6327
+4b3d30c691247c77.jpg",
 "https://telegra.ph/file/a2d01afe4f2cb1d4b650c.jpg",
 "https://telegra.ph/file/94dc035df11dfb159b999.jpg",
 "https://telegra.ph/file/fed9a5b1cbaaefc3a818c.jpg",
